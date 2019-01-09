387f424499b0aa9147e25990d638951446e85058
cyan <cyan24@my.bcit.ca>
2019-01-09 10:01:49 -0800

    Add project files.

---
+++ cy_mvc_08/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cy_mvc_08.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
